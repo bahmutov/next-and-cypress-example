1cfed10c6f67713d48c058dc912fa3810497f1bc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-08-18 11:11:27 -0400

    chore: use code coverage middleware

--- pages/api/__coverage__.js
+++ pages/api/__coverage__.js
@@ -1,12 +1,1 @@
-/**
- * Middleware for returning server-side code coverage
- * for Next.js API route.
- * @see https://nextjs.org/docs#api-routes
- * @see https://github.com/cypress-io/code-coverage
- */
-export default (req, res) => {
-  // only GET is supported
-  res.status(200).json({
-    coverage: global.__coverage__ || null
-  })
-}
+module.exports = require('@cypress/code-coverage/middleware/nextjs')
